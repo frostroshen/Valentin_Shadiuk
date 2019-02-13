2934b5d2aea78f397eb4ba9c7fdcffa56286e833
frostroshen <frostroshen@gmail.com>
2019-02-13 21:27:36 +0200

    js1

--- JavaScript/js/main2.js
+++ JavaScript/js/main2.js
@@ -1,4 +1,16 @@
-var t =' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at.'
+var arr1 = [];
+var t =' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at.';
 for (var i = 0; i <= t.length-1; i++ ){
-    console.log(t.indexOf('a',i));
- }
+        var b = arr1.push(t.indexOf('a',i));
+}
+function doSmth(arr1) {
+  for (var q=1, j=1; q<arr1.length; ++q) {
+    if (arr1[q] !== arr1[q-1]) {
+      arr1[j++] = arr1[q];
+    }
+  }
+
+  arr1.length = j;
+  return arr1;
+}
+console.log(doSmth(arr1))
